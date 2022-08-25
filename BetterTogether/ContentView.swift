b7bc7660dda6ab7fa660c25eee22c00c80447546
Daniel Alvarez <daniel@alvarez.tech>
2022-08-24 22:25:34 -0700

    Base UI

--- BetterTogether/ContentView.swift
+++ BetterTogether/ContentView.swift
@@ -8,9 +8,119 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedView = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selectedView) {
+            HomeScreen()
+                .tabItem {
+                    Label("Overview", systemImage: "chart.pie")
+                }
+                .tag(1)
+            ExpensesScreen()
+                .tabItem {
+                    Label("Expenses", systemImage: "list.bullet.rectangle.portrait")
+                }
+                .tag(2)
+        }
+    }
+}
+
+struct HomeScreen: View {
+    
+    @State private var showingAlert = false
+    @State private var showingSheet = false
+    
+    var body: some View {
+        NavigationView {
+            Text("Hello, world!")
+                .padding()
+                .navigationTitle("Holis")
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        Button("Add") {
+                            print("Added")
+                            showingSheet.toggle()
+                        }.sheet(isPresented: $showingSheet) {
+                            AddScreen()
+                        }
+                        Button("Other") {
+                            showingAlert = true
+                            print("other")
+                        }.alert("Holis", isPresented: $showingAlert) {
+                            Button("Okys") {}
+                        }
+                    }
+                }
+        }
+    }
+}
+
+struct ExpensesScreen: View {
+    var body: some View {
+        NavigationView {
+            List {
+                ExpenseView()
+                ExpenseView()
+                ExpenseView()
+            }
+            .navigationTitle("Expenses")
+        }
+    }
+}
+
+struct AddScreen: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var enableLogging = false
+    @State private var selectedColor = "Red"
+    @State private var colors = ["Red", "Green", "Blue"]
+    
+    @State private var title = ""
+    @State private var amount = ""
+    
+    var body: some View {
+        Form {
+            Section(footer: Text("Holis")) {
+                TextField("Title", text: $title)
+                    .font(.largeTitle)
+                
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+                
+                Picker("Category", selection: $selectedColor) {
+                    ForEach(colors, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                Toggle("Enable Logging", isOn: $enableLogging)
+            }
+            
+            Section {
+                Button("Close") {
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+struct ExpenseView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "fork.knife")
+            VStack(alignment: .leading) {
+                Text("Food")
+                Text("This is a description")
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+            Text("$ 4.12")
+                .font(.headline)
+        }
     }
 }
 
